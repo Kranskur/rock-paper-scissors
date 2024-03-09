a55c8665dc176cdc345f7fec85d3d728590b2a24
Roque <roquediazv1@gmail.com>
2024-03-09 13:19:20 -0300

    Added a function to play a round of Rock, Paper, Scissors

--- script.js
+++ script.js
@@ -13,3 +13,34 @@ function getComputerChoice(){
     }
 }
 
+// Make a function that plays a round of Rock Paper Scissors
+// Take two parameters
+// Return string that declares winner or tie of the round
+
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+    if(playerSelection == "rock" & computerSelection == "paper"){
+        return "You lose! Paper beats Rock"
+    }
+    else if(playerSelection == "rock" & computerSelection == "scissors"){
+        return "You win! Rock beats Scissors"
+    }
+    else if(playerSelection == computerSelection){
+        return "It's a tie"
+    }
+    else if(playerSelection == "paper" & computerSelection == "scissors"){
+        return "You lose! Scissors beats Paper"
+    }
+    else if(playerSelection == "paper" & computerSelection == "rock"){
+        return "You win! Paper beats Rock"
+    }
+    else if(playerSelection == "scissors" & computerSelection == "rock"){
+        return "You lose! Rock beats Scissors"
+    }
+    else if(playerSelection == "scissors" & computerSelection == "paper"){
+        return "You win! Scissors beats Paper"
+    }
+
+}
