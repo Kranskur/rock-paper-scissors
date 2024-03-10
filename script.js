df95f4eabe90bfee390864420ede2b97a6c508b3
Roque <roquediazv1@gmail.com>
2024-03-10 11:50:11 -0300

    Added a function to play a 5 round game of Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+let computerWin = 0;
+let playerWin = 0;
+let round = 1;
 // Make a function that randomly chooses between rock, paper and scissors
 function getComputerChoice(){
     choice = Math.floor(Math.random() * 3) + 1
@@ -22,25 +24,51 @@ function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase()
     computerSelection = computerSelection.toLowerCase()
     if(playerSelection == "rock" & computerSelection == "paper"){
+        computerWin++;
         return "You lose! Paper beats Rock"
     }
     else if(playerSelection == "rock" & computerSelection == "scissors"){
+        playerWin++;
         return "You win! Rock beats Scissors"
     }
     else if(playerSelection == computerSelection){
         return "It's a tie"
     }
     else if(playerSelection == "paper" & computerSelection == "scissors"){
+        computerWin++;
         return "You lose! Scissors beats Paper"
     }
     else if(playerSelection == "paper" & computerSelection == "rock"){
+        playerWin++;
         return "You win! Paper beats Rock"
     }
     else if(playerSelection == "scissors" & computerSelection == "rock"){
+        computerWin++;
         return "You lose! Rock beats Scissors"
     }
     else if(playerSelection == "scissors" & computerSelection == "paper"){
+        playerWin++;
         return "You win! Scissors beats Paper"
     }
 
 }
+//Make a function that plays a five round game, keeps score
+// and reports a winner or loser at the end
+function playGame(){
+    for(round; round <= 5; round++){
+        console.log(playRound(prompt("Choose rock, paper or scissors"),getComputerChoice()))
+        console.log("Number of rounds: ", round, "\n", playerWin,"-",computerWin)
+    }
+    if (playerWin > computerWin){
+        console.log("You won the game!")
+
+    }
+    else if (playerWin < computerWin){
+    console.log("You lost the game :(")
+}
+    else {
+    console.log("It's a tie")    
+    }
+}
+
+console.log(playGame())
